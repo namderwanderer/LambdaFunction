428802db3955e2f6556664e4ff8b750928ee0f58
Your Name <you@example.com>
2014-10-31 16:44:07 +0100

    gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
+#include "functional"
 
+template<typename lambda_function>
+int function(int**,lambda_function);
+
+template<typename lambda_function>
+int function(int ** arr, lambda_function lambdafunc)
+{
+
+}
 
 int main(){
 
